9b0731dcfaf18b893143eb023bce4d2ded4c1cdb
Jesus PM - UNIR <jesus.perezmelero@unir.net>
2025-08-29 13:40:12 +0200

    Update Spring Boot + Spring Cloud 2025.0.0 - WebFlux Starter

--- src/main/java/com/unir/gateway/GatewayAndFiltersApplication.java
+++ src/main/java/com/unir/gateway/GatewayAndFiltersApplication.java
@@ -1,10 +1,8 @@
 package com.unir.gateway;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
